f92be0cc837b8b80645b821587b970f3f34c66cc
ikoujar <ibrahimkoujardev@gmail.com>
2022-07-17 17:44:09 +0300

    Install Bootstrap and FE packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/globals.css';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </>
+  );
 }
 
-export default MyApp
+export default MyApp;
