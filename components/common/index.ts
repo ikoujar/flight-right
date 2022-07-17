0f86821fff29b64bc1f809dcec997ecaa463b995
ikoujar <ibrahimkoujardev@gmail.com>
2022-07-17 17:45:53 +0300

    Add general FE components.

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,9 +1,8 @@
 import Empty from './empty';
 import Error from './error';
 import Loading from './loading';
-import Pagination from './pagination';
 import SearchForm from './search';
 
 export {
-  Empty, Error, Loading, Pagination, SearchForm
+  Empty, Error, Loading, SearchForm
 };
